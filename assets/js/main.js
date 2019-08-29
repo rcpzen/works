841b43d40f8b86d92617e7784f7ece4b0f450ef8
Recep Özen <recepzen@hotmail.com>
2019-08-29 17:24:52 +0300

    vue/axios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
+var url = "https://jsonplaceholder.typicode.com/users"
+var data = []
+
+var app = new Vue({
+    el: '#discover',
+    data: {
+        products: []
+    },
+    created: function () {
+        this.getData();
+    },
+    methods: {
+        getData: function () {
+            axios.get(url).then(function (res) {
+                app.products = res.data;
+            }).catch(function (err) {
+                console.log(err)
+            })
+        }
+    }
+})
 $(document).ready(function () {
     var opt = {
         freeScroll: true,
